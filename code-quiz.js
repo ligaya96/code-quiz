a587e70c3cb8ef1b31d70c1fa6dfd1fe0e3dce23
ligaya <ligaya96@gmail.com>
2021-01-17 21:45:30 -0800

    added some JS content

--- code-quiz.js
+++ code-quiz.js
@@ -1,22 +1,35 @@
 var timer = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button")
 var coreectAnswers = document.querySelector("correctAnswerspan")
-var 
+var resetButton = document.querySelector(".resetButton")
+var correctAnswerspan = ""
 var rightAnswers = "";
 var wrongAnswers = "";
 var timerCount = "";
+//start of the quiz
 function startQuiz (){
-
+startButton.addEventListener("click", startQuiz)
 }
-function reset (){
-
-}
+// selecting right answers
 function click (){
 
 }
+//storing correctanswer points
 function saveAnswers(){
 
 }
+//wrong answer will reduce timer 
+function wrongAnswers(){
+
+}
+
+// retreive highscores and display them
 function retieveAnswers (){
     
 }
+// Reset the quiz
+function resetQuiz(){
+    rightAnswers= 0;
+    wrongAnswers=0;
+} 
+resetButton.addEventListener("click", resetQuiz);
