c63f6b8cdcb81e3327b84164008eae97273dff2b
ligaya <ligaya96@gmail.com>
2021-01-21 21:40:22 -0800

    adding some CSS/JS/HTML

--- js-quiz/script.js
+++ js-quiz/script.js
@@ -1,22 +1,31 @@
-var timer = document.querySelector(".timer-count");
-var startButton = document.getElementById("start-btn")
-var coreectAnswers = document.getElementById("correctAnswer")
-var wrongAnswers= document.getElementById("wrongAnswer")
-var resetButton = document.querySelector(".resetButton")
+var timerElement= document.querySelector(".timer-count");
+var startButton = document.getElementById("start-btn");
+var quizquestion = document.getElementById("question-content");
+var coreectAnswers = document.getElementById("correctAnswer");
+var wrongAnswers= document.getElementById("wrongAnswer");
+var resetButton = document.querySelector(".resetButton");
 var rightAnswers = "";
 var wrongAnswers = "";
-var timerCount = "";
+var timerCount;
+var timer;
+
 // click to begin the quiz
 startButton.addEventListener("click", startQuiz)
-//start of the quiz
-function startQuiz (){
-console.log("Started")
 
+//start of the quiz
+function startQuiz () {
+    startButton.disabled = true;
+    timerCount = 500;
+    quizquestion.classList.remove("hide");
+    //console.log ( "startQuiz")
+    startTimer()
+//console.log("Started")
 }
 // selecting right answers
 function selectAnwser(){
 
 }
+
 //storing correctanswer points
 function saveAnswers(){
 
@@ -29,6 +38,23 @@ function wrongAnswers(){
 // retreive highscores and display them
 function retieveAnswers (){
     
+}
+//timer
+function startTimer() {
+    timer = setInterval(function(){
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            //if (coreectAnswers && timercount > 0) {}
+            //clearInterval(timer);
+            //winQuiz();
+        }
+            if (timerCount ===0 ){
+                clearInterval(timer);
+                loseQuiz();
+            }
+        
+    },1000);
 }
 // Reset the quiz
 function resetQuiz(){
