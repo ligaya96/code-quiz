4df60d7a59b624d53de822f495d0e443b960b822
ligaya <ligaya96@gmail.com>
2021-01-22 02:14:00 -0800

    made major changes to code for easier JS

--- js-quiz/script.js
+++ js-quiz/script.js
@@ -1,28 +1,52 @@
 var timerElement= document.querySelector(".timer-count");
 var startButton = document.getElementById("start-btn");
 var quizquestion = document.getElementById("question-content");
-var coreectAnswers = document.getElementById("correctAnswer");
+var questionOne = document.querySelector("one");
+var questionTwo = document.getElementById("two");
+var questionThree = document.getElementById("three");
+var questionFour = document.getElementById("four")
+var correctAnswers = document.getElementById("correctAnswer");
 var wrongAnswers= document.getElementById("wrongAnswer");
 var resetButton = document.querySelector(".resetButton");
 var rightAnswers = "";
+var currentQuestion= "";
 var wrongAnswers = "";
 var timerCount;
 var timer;
-
+var questions = [ "one", "two", "three" , "four"]
 // click to begin the quiz
 startButton.addEventListener("click", startQuiz)
-
 //start of the quiz
 function startQuiz () {
     startButton.disabled = true;
     timerCount = 500;
-    quizquestion.classList.remove("hide");
-    //console.log ( "startQuiz")
-    startTimer()
-//console.log("Started")
+    currentQuestion= 0
+   quizquestion.classList.remove("hide")
+ //console.log ( "startQuiz")
+ //console.log (questionOne)
+    startTimer();
+   startQuestions();
 }
+// question#=1
+function startQuestions() { 
+    showQuestion([questionOne])
+   
+}
+function showQuestion(question) {
+    questionOne.textContent = question
+    //console.log(question)
+    //if correctAnswer button was pushed 
+    //then store correct answer 
+    if (question.correctAnswers) {
+        startButton.dataset.correctAnswers=quesion.correctAnswers
+
+    }
+    button.addEventListener("click", selectAnswer)
+}
+
 // selecting right answers
 function selectAnwser(){
+
 
 }
 
@@ -39,6 +63,15 @@ function wrongAnswers(){
 function retieveAnswers (){
     
 }
+
+
+//questions 
+//Console.log is useful for?
+// What is a CDN?
+//What are DOMS?
+//What does getElements by ID do?
+
+
 //timer
 function startTimer() {
     timer = setInterval(function(){
