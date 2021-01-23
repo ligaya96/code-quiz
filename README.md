bbd9830a4bc33eed75b286de4763e35134335fc2
ligaya <ligaya96@gmail.com>
2021-01-22 20:11:00 -0800

    Making changes to files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # code-quiz
+In this weeks homework. We were asked to make a Code quiz with a timer. If the user selected a wrong answer then the timer subtracted 10 seconds. At the end of the questions, there is an ablility to submit quiz. The user can then see their correct  answers. The user can then enter their initials, and be able to clear the quiz. 
--- js-quiz/script.js
+++ js-quiz/script.js
@@ -1,9 +1,11 @@
 var timerElement= document.querySelector(".timer-count");
 var startButton = document.getElementById("start-btn")
 var quizquestions = document.getElementById("Quiz");
-var quizcontent = document.getElementById( "question-content")
-var resetButton = document.querySelector(".resetButton");
-var questionButton = document.getElementById ("question-button")
+var quizcontent = document.getElementById( "question-content");
+var resetButton = document.getElementById("resetbutton");
+var nextButton = document.getElementById ("nextbutton" );
+var questionButton = document.getElementById ("question-buttons");
+var resultsScore = document.getElementById ("Results");
 var correctAnswers = true;
 var currentQuestion, shuffeledQuestion
 var wrongAnswers = "";
@@ -11,6 +13,10 @@ var timerCount;
 var timer;
 // click to begin the quiz
 startButton.addEventListener("click", startQuiz)
+nextButton.addEventListener("click", ()=> {
+    currentQuestion++
+    StartQuestion()
+})
 //start of the quiz
 function startQuiz () {
     startButton.classList.add("hide")
@@ -18,66 +24,93 @@ function startQuiz () {
     currentQuestion = 0
     shuffeledQuestion = questions.sort(() => Math.random() - .5)
     quizcontent.classList.remove("hide")
-    setNextQuestion();
+    StartQuestion();
     startTimer();
 }
-function setNextQuestion() {
+// Starting Questions
+function StartQuestion() {
+    resetQuestions()
     showQuestion(shuffeledQuestion[currentQuestion]);
 }
+// Showing the Questions
 function showQuestion(question){
-    quizquestions.innerHTML = question.question
+    quizquestions.innerText = question.question
+    question.answer.forEach(answer => {
+        var Button = document.createElement("button")
+        Button.innerText = answer.text
+        Button.classList.add("Btn")
+        //console.log (answer.text)
+        //storing correct answers
+        if (answer.correctAnswers) {
+          Button.dataset.correctAnswers = answer.correctAnswers
+        }
+        Button.addEventListener("click", RightAnwser);
+        questionButton.appendChild(Button);
+      console.log (questionButton)
+    });
 }
-
+// resetting original question buttons to actual questions, 
+function resetQuestions() {
+nextButton.classList.add("hide")
+resetButton.classList.add("hide")
+while (questionButton.firstChild) {
+    questionButton.removeChild(questionButton.firstChild)
+    }
+    
+}
 // selecting right answers
-//function selectAnwser(){
+function RightAnwser(e){
+    var clickedbutton = e.target
+    //var numcorrectAnswers= 0;
+    var correctAnswer = clickedbutton.dataset.correctAnswer
+if (shuffeledQuestion.length > currentQuestion + 1){
+nextButton.classList.remove("hide")
+} else {
+    startButton.innerHTML = "Submit"
+    resetButton.classList.remove("hide")
+}
+}
+//selecting wrong answer will reduce timer 
+function wrongAnswer(){
+    wrongAnswers = clickedbutton.correctAnswer;false
 
-
-//}
+}
 
 //storing correctanswer points
 //function saveAnswers(){
-
-//}
-//selecting wrong answer will reduce timer 
-//function wrongAnswers(){
-
 //}
 
-// retreive highscores and display them
-//function retieveAnswers (){
-    
-//}
+
 
 //the questions
 var questions = [
     {
         question: "What is a CDN?",
         answer: [
-            {a: "Content Delivery network", correctAnswer:true},
-            {b: "Contact Delivery network", correctAnswer:false},
-            {c: "Content Drama network", correctAnswer: false},
-            {d: "Content Delivery Netflix",coreectAnswer:false},
+            {text: "Content Delivery network", correctAnswer:true},
+            {text: "Contact Delivery network", correctAnswer:false},
+            {text: "Content Drama network", correctAnswer: false},
+            {text: "Content Delivery Netflix",coreectAnswer:false},
 
         ]
     },
     {
         question: "Console.log is useful for??",
         answer: [
-            {a: "Detecting", correctAnswer: false},
-            {b: "Debugging", correctAnswer: true},
-            {c: "Delivery", correctAnswer:  false},
-            {d: "Delonging", correctAnswer: false},
+            {text: "Detecting", correctAnswer: false},
+            {text: "Debugging", correctAnswer: true},
+            {text: "Delivery", correctAnswer:  false},
+            {text: "Delonging", correctAnswer: false},
 
         ]
     },
     {
-        question: "What are DOMS",
+        question: "What are DOMS?",
         answer: [
-            {a: "Dogs", correctAnswer: false},
-            {b: "Dental", correctAnswer: false},
-            {c: "Document Object Model", correctAnswer:  true},
-            {d: "Delonging", correctAnswer
-            : false},
+            {text: "Dogs", correctAnswer: false},
+            {text: "Dental", correctAnswer: false},
+            {text: "Document Object Model", correctAnswer:  true},
+            {text: "Delonging", correctAnswer: false},
         ]
     }
 ]
@@ -88,20 +121,21 @@ function startTimer() {
         timerCount--;
         timerElement.textContent = timerCount;
         if (timerCount >= 0) {
-            //if (coreectAnswers && timercount > 0) {}
-            //clearInterval(timer);
+    if (CorrectAnswers && timerCount > 0) {}
+            clearInterval(timer);
             //winQuiz();
         }
             if (timerCount ===0 ){
                 clearInterval(timer);
-                loseQuiz();
+               // loseQuiz();
             }
         
     },1000);
 }
-// Reset the quiz
-function resetQuiz(){
-    rightAnswers= 0;
-    wrongAnswers=0;
-} 
+// retreive highscores and display them
+//function retieveAnswers (){
+
+    
+//}
+
 //resetButton.addEventListener("click", resetQuiz)
